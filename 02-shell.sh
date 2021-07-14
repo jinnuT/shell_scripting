45015cb77cac3c35a800a3b3a990db8f3d52c494
jinnuT <jinnuaz@hotmail.com>
2021-07-13 17:47:37 -0700

    playing with codes. especially coloring enabling and disabling them

--- 02-shell.sh
+++ 02-shell.sh
@@ -1,3 +1,30 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-echo "Hello World"
+echo Hello World
+# printing message in multiple lines
+echo -e "Hello, \n\tWorld"
+
+# syntax for enabling the color
+#--------------------------------
+# echo -e "\e[COLOURCODEmMESSAGE"
+# REED    31
+# GREEN   32
+# YELLOW  33
+# BLUE    34
+# MAGENTA 35
+# CYAN    36
+
+echo testing color without disabling them
+echo ------------------------------------
+echo -e "\e[31mJinnu in RED"
+echo writing some plain text
+echo -e "\e[35nJinnu in MAGENTA"
+echo writing some plain text
+
+
+echo testing color with disabling them
+echo ------------------------------------
+echo -e "\e[31mJinnu in RED\e[0m"
+echo writing some plain text
+echo -e "\e[35nJinnu in MAGENTA\e[0m"
+echo writing some plain text
