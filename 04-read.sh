2f7e238070047dcf43d16d37d441dc4b0fa35492
jinnuT <jinnuaz@hotmail.com>
2021-07-14 15:20:29 -0700

    Reading the inputs

--- 04-read.sh
+++ 04-read.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/bash
-
+echo "***********************************************************"
 read -p 'Enter your name: ' name
 echo "Your name is $name"
 
 read -p 'Enter your age: ' age
+echo "Your age is $age"
+
+echo "***********************************************************"
+
+
+read -p "Enter your name: " name
+read -p "Enter your Age: " age
+echo "Your name is $name"
 echo "Your age is $age"
