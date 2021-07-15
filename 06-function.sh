1bcddab1a7ca5f7690abad111beec2f9790ab40a
jinnuT <jinnuaz@hotmail.com>
2021-07-14 23:04:46 -0700

    maintainance

--- 06-function.sh
+++ 06-function.sh
@@ -10,16 +10,14 @@ sample(){
 
 echo b=$b
 num=2
-for var in 1 2 3
+for var in 1 2
 do
-  echo "var = "$var
   sample
 done
 
 while [ $num -le 10 ]
 do
   sample
-  echo "num = "$num
   num=$(($num+1))
 
 done
