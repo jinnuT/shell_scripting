ae06f5cb15fc8284c838450e2dadad1d9bed3769
jinnuT <jinnuaz@hotmail.com>
2021-07-15 09:12:43 -0700

    updating 3

--- 06-function.sh
+++ 06-function.sh
@@ -13,22 +13,24 @@ echo first argument inside the main function = $1
 echo second argument inside the main function = $2
 
 ##----Accessing variables declared inside the function and outside (main)-------------------------
-#num=6
-#sample
-#echo value of b=$b
+num=6
+sample
+echo value of b=$b
 
 ##-----LOOPS----------------------------------------------------------------------------------------
 
-#echo b=$b
-#num=2
-#for var in 1 2
-#do
-#  sample
-#done
+echo b=$b
+num=7
+for var in 1 2
+do
+  sample
+done
 
-#while [ $num -le 10 ]
-#do
-#  sample
-#  num=$(($num+1))
+echo "--------------------------------------------------------------------------------------------"
 
-#done
+while [ $num -le 10 ]
+do
+  sample
+  num=$(($num+1))
+
+done
