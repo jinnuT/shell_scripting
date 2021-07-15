e63be9d25227be62c4c84bd8b96939331a3dee2c
jinnuT <jinnuaz@hotmail.com>
2021-07-14 22:39:34 -0700

    Updated the code with assigning function call to a variable var

--- 06-function.sh
+++ 06-function.sh
@@ -6,14 +6,17 @@ sample(){
   echo "Hello, I'm a sample function"
 }
 
-num=5
+num=2
 for var in 1 2 3
 do
+  echo "var = "$var
   sample
 done
 
 while [ $num -le 10 ]
 do
   sample
+  echo "num = "$num
   num=$(($num+1))
+
 done
