b6ed55727e2e7eb3334018928d3087695c72aa11
jinnuT <jinnuaz@hotmail.com>
2021-07-17 11:04:37 -0700

    output and error (if have one) will going to the same file

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,15 +3,15 @@ LOG=/tmp/roboshop.log
 rm -f $LOG
 
 echo -e "Installing Nginx\t\t.....\t\e[32mdone\e[0m"
-yum install nginx -y >>$LOG
+yum install nginx -y &>>$LOG
 
 echo -e "Enabling Nginx\t\t\t.....\t\e[32mdone\e[0m"
-systemctl enable nginx >>$LOG
+systemctl enable nginx &>>$LOG
 
 echo -e "Starting Nginx\t\t\t.....\t\e[32mdone\e[0m"
-systemctl start nginx >>$LOG
+systemctl start nginx &>>$LOG
 
 echo -e "Checking Status Nginx\t\t.....\t\e[32mdone\e[0m"
-systemctl status nginx >>$LOG
+systemctl status nginx &>>$LOG
 
 
