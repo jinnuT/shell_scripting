1b1f2d58cdade4ba30925bfdccf91f48f60ec14d
jinnuT <jinnuaz@hotmail.com>
2021-07-17 12:39:41 -0700

    Introduces exit commands in frontend

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -4,14 +4,18 @@ rm -f $LOG
 
 echo -e "Installing Nginx\t\t.....\t\e[32mdone\e[0m"
 yum install nginx -y &>>$LOG
+echo $?
 
 echo -e "Enabling Nginx\t\t\t.....\t\e[32mdone\e[0m"
 systemctl enable nginx &>>$LOG
+echo $?
 
 echo -e "Starting Nginx\t\t\t.....\t\e[32mdone\e[0m"
 systemctl start nginx &>>$LOG
+echo $?
 
 echo -e "Checking Status Nginx\t\t.....\t\e[32mdone\e[0m"
 systemctl status nginx &>>$LOG
+echo $?
 
 
