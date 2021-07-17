3c7739f40df791f91d5bff29847a21cb03f4be67
jinnuT <jinnuaz@hotmail.com>
2021-07-17 10:25:43 -0700

    concatenating all the output messages

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -2,15 +2,15 @@
 LOG=/tmp/roboshop.log
 
 echo -e "Installing Nginx\t\t.....\t\e[32mdone\e[0m"
-yum install nginx -y >$LOG
-
+yum install nginx -y >>$LOG
+echo "-----------------------------------------------------"
 echo -e "Enabling Nginx\t\t\t.....\t\e[32mdone\e[0m"
-systemctl enable nginx >$LOG
-
+systemctl enable nginx >>$LOG
+echo "-----------------------------------------------------"
 echo -e "Starting Nginx\t\t\t.....\t\e[32mdone\e[0m"
-systemctl start nginx >$LOG
-
-#echo -e "Checking Status Nginx\t\t.....\t\e[32mdone\e[0m"
-systemctl status nginx >$LOG
+systemctl start nginx >>$LOG
+echo "-----------------------------------------------------"
+echo -e "Checking Status Nginx\t\t.....\t\e[32mdone\e[0m"
+systemctl status nginx >>$LOG
 
 
