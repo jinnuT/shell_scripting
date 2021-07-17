a070e6cd437e9023740ef48800769aba0ebf89a0
jinnuT <jinnuaz@hotmail.com>
2021-07-17 10:18:20 -0700

    Introducing -e

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/bash
 LOG=/tmp/roboshop.log
 
-echo "Installing Nginx\t\t.....\t\e[32mdone\e[0m"
+echo -e "Installing Nginx\t\t.....\t\e[32mdone\e[0m"
 yum install nginx -y >$LOG
 
-echo "Enabling Nginx\t\t.....\t\e[32mdone\e[0m"
+echo -e "Enabling Nginx\t\t.....\t\e[32mdone\e[0m"
 systemctl enable nginx >$LOG
 
-echo "Starting Nginx\t\t.....\t\e[32mdone\e[0m"
+echo -e "Starting Nginx\t\t.....\t\e[32mdone\e[0m"
 systemctl start nginx >$LOG
 
-echo "Checking Status Nginx\t\t.....\t\e[32mdone\e[0m"
+echo -e "Checking Status Nginx\t\t.....\t\e[32mdone\e[0m"
 systemctl status nginx >$LOG
 
 
