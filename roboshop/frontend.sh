ac1fe75540e367e036767bf613f7488d878da82b
jinnuT <jinnuaz@hotmail.com>
2021-07-17 10:19:47 -0700

    Introducing leveling outputs

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -4,10 +4,10 @@ LOG=/tmp/roboshop.log
 echo -e "Installing Nginx\t\t.....\t\e[32mdone\e[0m"
 yum install nginx -y >$LOG
 
-echo -e "Enabling Nginx\t\t.....\t\e[32mdone\e[0m"
+echo -e "Enabling Nginx\t\t\t.....\t\e[32mdone\e[0m"
 systemctl enable nginx >$LOG
 
-echo -e "Starting Nginx\t\t.....\t\e[32mdone\e[0m"
+echo -e "Starting Nginx\t\t\t.....\t\e[32mdone\e[0m"
 systemctl start nginx >$LOG
 
 echo -e "Checking Status Nginx\t\t.....\t\e[32mdone\e[0m"
