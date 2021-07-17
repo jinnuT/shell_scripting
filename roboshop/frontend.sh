5e2b4e66c08bd3a83916e5d5082cb3a4592e4ac3
jinnuT <jinnuaz@hotmail.com>
2021-07-17 10:40:40 -0700

    deleting the message already in the file or Displaying the latest messages.

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/bash
 LOG=/tmp/roboshop.log
+rm -f $LOG
 
 echo -e "Installing Nginx\t\t.....\t\e[32mdone\e[0m"
 yum install nginx -y >>$LOG
-echo "-----------------------------------------------------" >>$LOG
+
 echo -e "Enabling Nginx\t\t\t.....\t\e[32mdone\e[0m"
 systemctl enable nginx >>$LOG
-echo "-----------------------------------------------------" >>$LOG
+
 echo -e "Starting Nginx\t\t\t.....\t\e[32mdone\e[0m"
 systemctl start nginx >>$LOG
-echo "-----------------------------------------------------" >>$LOG
+
 echo -e "Checking Status Nginx\t\t.....\t\e[32mdone\e[0m"
 systemctl status nginx >>$LOG
 
