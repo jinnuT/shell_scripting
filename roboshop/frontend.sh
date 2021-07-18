01c3db264db32c79982d5667cca9bf13a8f96ce2
jinnuT <jinnuaz@hotmail.com>
2021-07-17 18:08:26 -0700

    update2

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -14,22 +14,22 @@ fi
 ###echo -e "Enabling Nginx\t\t\t.....\t\e[32mdone\e[0m"
 systemctl enable nginx &>>$LOG
 if [ $? -eq 0 ]; then
-  echo ".....\e[32m ngnix Successfully Enabled\e[0m"
+  echo ".....\e[32mngnix_Successfully_Enabled\e[0m"
 else
-  echo ".....\e[32m nginx Enabling Failed\e[0m"
+  echo ".....\e[32mnginx_Enabling_Failed\e[0m"
 fi
 
 ###echo -e "Starting Nginx\t\t\t.....\t\e[32mdone\e[0m"
 systemctl start nginx &>>$LOG
 if [ $? -eq 0 ]; then
-  echo ".......\e[32m nginx Successfully Started\e[0m"
+  echo ".......\e[32mnginx_Successfully_Started\e[0m"
 else
-  echo "......\e[32m nginx Starting Failed\e[0m"
+  echo "......\e[32mnginx_Starting_Failed\e[0m"
 fi
 ###echo -e "Checking Status Nginx\t\t.....\t\e[32mdone\e[0m"
 systemctl status nginx &>>$LOG
 if [ $? -eq 0 ]; then
-  echo ".....\e[32m nginx Status Checking\e[0m"
+  echo ".....\e[32mnginx_Status_Checking\e[0m"
 else
-  echo ".....\e[32m nginx Status Checking Failed\e[0m"
+  echo ".....\e[32mnginx_Status_Checking_Failed\e[0m"
 fi
