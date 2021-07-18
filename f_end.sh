ea992e32e65f11d0469c208e23bc742532c9060e
jinnuT <jinnuaz@hotmail.com>
2021-07-18 10:48:02 -0700

    This is just a practice

---
+++ f_end.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+LOG=/tmp/rbshop.log
+
+
+yum install nginx -y    &>$LOG
+systemctl enable nginx  &>$LOG
+systemctl start nginx   &>$LOG
+
+
+
+
+## 1) Cleaning up all the output and error message showing in the terminal
+##      - use the redirect (> or &> or 1> or 2>) the log message to a desired folder
