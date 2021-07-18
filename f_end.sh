63015929061c895c9e08fd24bc1c8f7bab0f5dd8
jinnuT <jinnuaz@hotmail.com>
2021-07-18 11:22:36 -0700

    status2

--- f_end.sh
+++ f_end.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/bash
 
 LOG=/tmp/rbshop.log
+rm -f $LOG
 
-
+echo -e "\t\t\e[32mInstalling Nginx....\t\e[0m"
 yum install nginx -y >>$LOG
+echo -e "\t\t\e[32mEnabling Nginx....\t\e[0m"
 systemctl enable nginx >>$LOG
+echo -e "\t\t\e[32mStarting Nginx....\t\e[0m"
 systemctl start nginx >>$LOG
-systemctl status nginx >>$LOG
+#################################################systemctl status nginx >>$LOG
 
 
 
 
 ## 1) Cleaning up all the output and error message showing in the terminal
-##      - use the redirect (> or &> or 1> or 2>) the log message to a desired folder
+##    -- Output from command should not be displayed on the screen
+##    -- use the redirect (> or &> or 1> or 2>) the log message to a desired folder
