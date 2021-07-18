60279fd3ac330505b8b8e36ceb9f8c17e080ab74
jinnuT <jinnuaz@hotmail.com>
2021-07-18 15:11:15 -0700

    USER_ID FUNCTION upd1

--- common.sh
+++ common.sh
@@ -17,6 +17,6 @@ PRINT(){
 
 USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]; then
-  echo -e "\e[32mYou should be a root user or sudo user to coninue with the script\e[0m"
+  echo -e "\e[31mYou should be a root user or sudo user to coninue with the script\e[0m"
   exit 2
 fi
