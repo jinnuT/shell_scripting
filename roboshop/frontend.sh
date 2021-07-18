c66b6d516c829bb549bc3dae2dd32aae2e63e1da
jinnuT <jinnuaz@hotmail.com>
2021-07-18 16:24:10 -0700

    Roboshop.sh

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,62 +1,40 @@
 #!/usr/bin/bash
-LOG=/tmp/roboshop.log
-rm -f $LOG                    ##### Deleting exiting log already saved inside
+source common.sh
 
-PRINT(){
-  echo -e "\t..."$1
-}
+PRINT "Installing Nginx\t"
+yum install nginx -y &>$LOG
+STAT_CHECK $?
 
-STAT_CHECK1() {
-  if [ $1 -eq 0 ]; then
-    echo -e "\e[32mnginx Successfully Installed\e[0m"
-  else
-    echo -e "\e[31mnginx Installation Failed\e[0m"
-    exit 1
-  fi
-}
+PRINT "Download Frontend"
+curl -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>$LOG
+STAT_CHECK $?
 
-STAT_CHECK2() {
+PRINT "Remove all the old HTDocs"
+cd /usr/share/nginx/html &>$LOG && rm -rf * &>$LOG
+STAT_CHECK $?
 
-  if [ $1 -eq 0 ]; then
-    echo -e "\e[32mngnix Successfully Enabled\e[0m"
-  else
-    echo -e "\e[31mnginx Enabling Failed\e[0m"
-    exit 1
-  fi
-}
+PRINT "Extract Frontend Archives"
+unzip /tmp/frontend.zip &>$LOG && mv frontend-main/* . &>$LOG && mv static/* . &>$LOG && rm -rf frontend-master static &>$LOG
 
-STAT_CHECK3() {
+PRINT "Update Roboshop Config"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>$LOG
 
-  if [ $1 -eq 0 ]; then
-    echo -e "\e[32mngnix Successfully Started\e[0m"
-  else
-    echo -e "\e[31mnginx Start Failed\e[0m"
-    exit 1
-  fi
-}
 
-STAT_CHECK4() {
-  if [ $1 -eq 0 ]; then
-    echo -e "\e[32mnginx Status Checked succesfully\e[0m"
-  else
-    echo -e "\e[31mnginx Status Checking Failed\e[0m"
-    exit 1
-  fi
-}
+PRINT "Enabling Nginx\t\t"
+systemctl enable nginx &>$LOG
+STAT_CHECK $?
 
-###echo -e "Installing Nginx\t\t.....\t\e[32mdone\e[0m"
-yum install nginx -y &>>$LOG  ##### &> means attaching both output and error in one file
-#echo $?                       ##### Exit command will display 0 if it is successful
-STAT_CHECK1 $?
+PRINT "Starting Nginx\t\t"
+systemctl restart nginx &>$LOG
+STAT_CHECK $?
 
-###echo -e "Enabling Nginx\t\t\t.....\t\e[32mdone\e[0m"
-systemctl enable nginx &>>$LOG
-STAT_CHECK2 $?
 
-###echo -e "Starting Nginx\t\t\t.....\t\e[32mdone\e[0m"
-systemctl start nginx &>>$LOG
-STAT_CHECK3 $?
 
-###echo -e "Checking Status Nginx\t\t.....\t\e[32mdone\e[0m"
-systemctl status nginx &>>$LOG
-STAT_CHECK4 $?
+#################################################systemctl status nginx >>$LOG
+
+
+
+
+## 1) Cleaning up all the output and error message showing in the terminal
+##    -- Output from command should not be displayed on the screen
+##    -- use the redirect (> or &> or 1> or 2>) the log message to a desired folder
