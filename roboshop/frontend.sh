30e0c04e65291c1666f5112138dfd793acc20109
jinnuT <jinnuaz@hotmail.com>
2021-07-18 16:30:16 -0700

    Roboshop.sh update3

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 source common.sh
 
-PRINT "Installing Nginx\t"
+PRINT "Installing Nginx\t\t"
 yum install nginx -y &>$LOG
 STAT_CHECK $?
 
@@ -9,11 +9,11 @@ PRINT "Download Frontend\t\t"
 curl -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>$LOG
 STAT_CHECK $?
 
-PRINT "Remove all the old HTDocs\t\t"
+PRINT "Remove all the old HTDocs\t"
 cd /usr/share/nginx/html &>$LOG && rm -rf * &>$LOG
 STAT_CHECK $?
 
-PRINT "Extract Frontend Archives\t\t"
+PRINT "Extract Frontend Archives\t"
 unzip /tmp/frontend.zip &>$LOG && mv frontend-main/* . &>$LOG && mv static/* . &>$LOG && rm -rf frontend-master static &>$LOG
 STAT_CHECK $?
 
@@ -21,11 +21,11 @@ PRINT "Update Roboshop Config\t\t"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>$LOG
 STAT_CHECK $?
 
-PRINT "Enabling Nginx\t\t"
+PRINT "Enabling Nginx\t\t\t"
 systemctl enable nginx &>$LOG
 STAT_CHECK $?
 
-PRINT "Starting Nginx\t\t"
+PRINT "Starting Nginx\t\t\t"
 systemctl restart nginx &>$LOG
 STAT_CHECK $?
 
