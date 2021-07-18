3e6903f9af5884fe5fd0782b8504a4ce578dbf3b
jinnuT <jinnuaz@hotmail.com>
2021-07-18 13:51:33 -0700

    makeup code2

--- f_end.sh
+++ f_end.sh
@@ -11,7 +11,7 @@ else
   echo -e "\e[31mfail..\e[0m"
   exit 1
 fi
-echo -n -e "Enabling Nginx\t"
+echo -n -e "\tEnabling Nginx\t"
 systemctl enable nginx >>$LOG
 if [ $? -eq 0 ]; then
   echo -e "\e[32mdone..\e[0m"
@@ -19,7 +19,7 @@ else
   echo -e "\e[31mfail..\e[0m"
   exit 1
 fi
-echo -n -e "Starting Nginx\t"
+echo -n -e "\tStarting Nginx\t"
 systemctl start nginx >>$LOG
 if [ $? -eq 0 ]; then
   echo -e "\e[32mdone..\e[0m"
