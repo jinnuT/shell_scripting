0ad1fbb188e735de418f324f9cc0cd210e92e6d4
jinnuT <jinnuaz@hotmail.com>
2021-07-17 18:30:07 -0700

    error checking intro exit 1

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -9,6 +9,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mnginx Successfully Installed\e[0m"
 else
   echo -e "\e[31mnginx Installation Failed\e[0m"
+  exit 1
 fi
 
 ###echo -e "Enabling Nginx\t\t\t.....\t\e[32mdone\e[0m"
@@ -17,6 +18,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mngnix Successfully Enabled\e[0m"
 else
   echo -e "\e[31mnginx Enabling Failed\e[0m"
+  exit 1
 fi
 
 ###echo -e "Starting Nginx\t\t\t.....\t\e[32mdone\e[0m"
@@ -25,6 +27,7 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mnginx Successfully Started\e[0m"
 else
   echo -e "\e[31mnginx Starting Failed\e[0m"
+  exit 1
 fi
 ###echo -e "Checking Status Nginx\t\t.....\t\e[32mdone\e[0m"
 systemctl status nginx &>>$LOG
@@ -32,4 +35,5 @@ if [ $? -eq 0 ]; then
   echo -e "\e[32mnginx Status Checked succesfully\e[0m"
 else
   echo -e "\e[31mnginx Status Checking Failed\e[0m"
+  exit 1
 fi
