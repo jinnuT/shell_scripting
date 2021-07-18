4a5d3f90c5ba11367288e89bdd4d45a20bebec24
jinnuT <jinnuaz@hotmail.com>
2021-07-17 18:03:30 -0700

    introduced if else

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -1,21 +1,35 @@
 #!/usr/bin/bash
 LOG=/tmp/roboshop.log
-rm -f $LOG
+rm -f $LOG                    ##### Deleting exiting log already saved inside
 
-echo -e "Installing Nginx\t\t.....\t\e[32mdone\e[0m"
-yum install nginx -y &>>$LOG
-echo $?
+###echo -e "Installing Nginx\t\t.....\t\e[32mdone\e[0m"
+yum install nginx -y &>>$LOG  ##### &> means attaching both output and error in one file
+#echo $?                       ##### Exit command will display 0 if it is successful
+if [ $? -eq 0 ]; then
+  echo ".....\e[32m nginx Successfully Installed\e[0m"
+else
+  echo ".....\e[32m nginx Installation Failed\e[0m"
+fi
 
-echo -e "Enabling Nginx\t\t\t.....\t\e[32mdone\e[0m"
+###echo -e "Enabling Nginx\t\t\t.....\t\e[32mdone\e[0m"
 systemctl enable nginx &>>$LOG
-echo $?
+if [ $? -eq 0 ]; then
+  echo ".....\e[32m ngnix Successfully Enabled\e[0m"
+else
+  echo ".....\e[32m nginx Enabling Failed\e[0m"
+fi
 
-echo -e "Starting Nginx\t\t\t.....\t\e[32mdone\e[0m"
+###echo -e "Starting Nginx\t\t\t.....\t\e[32mdone\e[0m"
 systemctl start nginx &>>$LOG
-echo $?
-
-echo -e "Checking Status Nginx\t\t.....\t\e[32mdone\e[0m"
+if [ $? -eq 0 ]; then
+  echo ".......\e[32m nginx Successfully Started\e[0m"
+else
+  echo "......\e[32m nginx Starting Failed\e[0m"
+fi
+###echo -e "Checking Status Nginx\t\t.....\t\e[32mdone\e[0m"
 systemctl status nginx &>>$LOG
-echo $?
-
-
+if [ $? -eq 0 ]; then
+  echo ".....\e[32m nginx Status Checking\e[0m"
+else
+  echo ".....\e[32m nginx Status Checking Failed\e[0m"
+fi
