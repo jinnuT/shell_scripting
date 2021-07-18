920a489eeacb9f8fff6488bd48e0a93ede611ce7
jinnuT <jinnuaz@hotmail.com>
2021-07-18 13:50:47 -0700

    makeup code

--- f_end.sh
+++ f_end.sh
@@ -3,7 +3,7 @@
 LOG=/tmp/rbshop.log
 rm -f $LOG
 
-echo -n -e "Installing Nginx\t\t"
+echo -n -e "Installing Nginx\t"
 yum install nginx -y >>$LOG
 if [ $? -eq 0 ]; then
   echo -e "\e[32mdone..\e[0m"
@@ -11,7 +11,7 @@ else
   echo -e "\e[31mfail..\e[0m"
   exit 1
 fi
-echo -n -e "Enabling Nginx\t\t"
+echo -n -e "Enabling Nginx\t"
 systemctl enable nginx >>$LOG
 if [ $? -eq 0 ]; then
   echo -e "\e[32mdone..\e[0m"
@@ -19,7 +19,7 @@ else
   echo -e "\e[31mfail..\e[0m"
   exit 1
 fi
-echo -n -e "Starting Nginx\t\t"
+echo -n -e "Starting Nginx\t"
 systemctl start nginx >>$LOG
 if [ $? -eq 0 ]; then
   echo -e "\e[32mdone..\e[0m"
