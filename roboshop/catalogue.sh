ad1c1f59d32f75a2766a145a7661b259abd590e6
jinnuT <jinnuaz@hotmail.com>
2021-07-20 10:43:30 -0700

    update 5

--- roboshop/catalogue.sh
+++ roboshop/catalogue.sh
@@ -3,11 +3,11 @@
 source common.sh
 
 PRINT "Install nodejs\t\t"
-yum install nodejs make gcc-c++ -y &>$LOG
+yum install nodejs make gcc-c++ -y &>>$LOG
 STAT_CHECK $?
 
 PRINT "Add Roboshop Application User "
-useradd roboshop &>$LOG
+useradd roboshop &>>$LOG
 STAT_CHECK $?
 #catalo
 #$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
