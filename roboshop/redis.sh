f6eb4b822a357bad95df95217361d691fd576b18
jinnuT <jinnuaz@hotmail.com>
2021-07-20 16:15:00 -0700

    redis installing

--- roboshop/redis.sh
+++ roboshop/redis.sh
@@ -1,1 +1,19 @@
-ECHO is on.
+#!/usr/bin/bash
+
+source common.sh
+
+PRINT "Install Redis Repos"
+yum install epel-release yum-utils http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
+STAT_CHECK $?
+
+PRINT "Install Redis\t"
+# yum-config-manager --enable remi
+yum install redis -y --enablerepo=remi &>>$LOG
+STAT_CHECK $?
+
+#Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
+
+#Start Redis Database
+
+# systemctl enable redis
+# systemctl start redis
