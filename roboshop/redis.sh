65a3f7ef6bebc993e4a9bb0ed8ddaaf537a3ba7e
jinnuT <jinnuaz@hotmail.com>
2021-07-20 16:28:29 -0700

    redis installing update

--- roboshop/redis.sh
+++ roboshop/redis.sh
@@ -11,9 +11,11 @@ PRINT "Install Redis\t"
 yum install redis -y --enablerepo=remi &>>$LOG
 STAT_CHECK $?
 
+PRINT "Update Redia Listen Address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+STAT_CHECK $?
 #Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
 
-#Start Redis Database
-
-# systemctl enable redis
-# systemctl start redis
+PRINT "Start Redis Service"
+systemctl enable redis && systemctl start redis &>>$LOG
+STAT_CHECK $?
