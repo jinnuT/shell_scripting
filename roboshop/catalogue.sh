d7f0f8a06b33595642723d1aed82175c67dbec94
jinnuT <jinnuaz@hotmail.com>
2021-07-20 10:56:05 -0700

    update both catalogue and common 2

--- roboshop/catalogue.sh
+++ roboshop/catalogue.sh
@@ -7,7 +7,10 @@ yum install nodejs make gcc-c++ -y &>>$LOG
 STAT_CHECK $?
 
 PRINT "Add Roboshop Application User "
-useradd roboshop &>>$LOG
+id roboshop &>>$LOG
+if [ $? -ne 0 ]; then
+  useradd roboshop &>>$LOG
+fi
 STAT_CHECK $?
 #catalologu
 #$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
