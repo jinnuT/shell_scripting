9b0572fe86990e560a61f298219ca9f993b0884e
jinnuT <jinnuaz@hotmail.com>
2021-07-20 10:22:56 -0700

    installing catalogue

--- roboshop/catalogue.sh
+++ roboshop/catalogue.sh
@@ -1,1 +1,18 @@
-ECHO is on.
+#!/usr/bin/bash
+
+source common.sh
+
+PRINT "Install nodejs\t\t"
+yum install nodejs make gcc-c++ -y &>$LOG
+STAT_CHECK $?
+
+PRINT "Add Roboshop Application User"
+useradd roboshop &>$LOG
+STAT_CHECK $?
+
+#$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+#$ cd /home/roboshop
+#$ unzip /tmp/catalogue.zip
+#$ mv catalogue-main catalogue
+#$ cd /home/roboshop/catalogue
+#$ npm install
