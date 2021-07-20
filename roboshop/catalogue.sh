89dbe4e7e7218cdc3004c766a0e7c1e913f80f29
jinnuT <jinnuaz@hotmail.com>
2021-07-20 12:45:13 -0700

    set up both Download Application Code and Extract Downloaded code

--- roboshop/catalogue.sh
+++ roboshop/catalogue.sh
@@ -12,10 +12,14 @@ if [ $? -ne 0 ]; then
   useradd roboshop &>>$LOG
 fi
 STAT_CHECK $?
-#catalologu
-#$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
-#$ cd /home/roboshop
-#$ unzip /tmp/catalogue.zip
-#$ mv catalogue-main catalogue
-#$ cd /home/roboshop/catalogue
-#$ npm install
+
+
+PRINT "Download Application Code"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
+STAT_CHECK $?
+
+
+PRINT "Extract Downloaded code"
+cd /home/roboshop && unzip /tmp/catalogue.zip &>>$LOG && mv catalogue-main catalogue && cd /home/roboshop/catalogue && npm install
+STAT_CHECK $?
+
