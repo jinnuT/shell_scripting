e04dbb3c2e6f6d10805596130cd40959aa41b760
jinnuT <jinnuaz@hotmail.com>
2021-07-21 16:16:04 -0700

    update 8

--- roboshop/mysql.sh
+++ roboshop/mysql.sh
@@ -2,7 +2,7 @@
 
 source common.sh
 
-PRINT "Setup MySQL Repos"
+PRINT "Setup MySQL Repos\t"
 echo '[mysql57-community]
 name=MySQL 5.7 Community Server
 baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/
@@ -10,11 +10,11 @@ enabled=1
 gpgcheck=0' > /etc/yum.repos.d/mysql.repo
 STAT_CHECK $?
 
-PRINT "Install MySQL\t"
+PRINT "Install MySQL\t\t"
 yum install mysql-community-server -y &>>$LOG
 STAT_CHECK $?
 
-PRINT "Start MySQL Service"
+PRINT "Start MySQL Service\t"
 systemctl enable mysqld &>>$LOG && systemctl start mysqld &>>$LOG
 STAT_CHECK $?
 
@@ -35,11 +35,11 @@ fi
 STAT_CHECK $?
 
 
-PRINT "Download Schema"
+PRINT "Download Schema\t\t"
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>$LOG
 STAT_CHECK $?
 
-PRINT "Load Schema"
+PRINT "Load Schema\t\t"
 cd /tmp && unzip -o mysql.zip &>>$LOG && cd mysql-main && mysql -uroot -pRoboShop@1 <shipping.sql &>>$LOG
 STAT_CHECK $?
 
