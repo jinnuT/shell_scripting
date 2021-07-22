a33a3b05a6a25e1765118dd49c9c6d160a6ed877
jinnuT <jinnuaz@hotmail.com>
2021-07-22 11:59:15 -0700

    f_end.sh Loops - Intro

--- f_end.sh
+++ f_end.sh
@@ -1,22 +1,8 @@
 #!/usr/bin/bash
-source common.sh
 
-PRINT "Installing Nginx\t"
-yum install nginx -y >>$LOG
-STAT_CHECK $?
+PRINT() {
+  echo -e "\n###########################\t$1\t###########################" &>>$LOG
+  echo -n -e "$1\t\t..."
+}
 
-PRINT "Enabling Nginx\t\t"
-systemctl enable nginx >>$LOG
-STAT_CHECK $?
-
-PRINT "Starting Nginx\t\t"
-systemctl start nginx >>$LOG
-STAT_CHECK $?
-#################################################systemctl status nginx >>$LOG
-
-
-
-
-## 1) Cleaning up all the output and error message showing in the terminal
-##    -- Output from command should not be displayed on the screen
-##    -- use the redirect (> or &> or 1> or 2>) the log message to a desired folder
+PRINT "WHILE LOOP"
