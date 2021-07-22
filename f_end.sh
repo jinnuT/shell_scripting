37396ea99f05124993333a7f33ae06261de78d2a
jinnuT <jinnuaz@hotmail.com>
2021-07-22 12:22:31 -0700

    f_end.sh intro - Status Check

--- f_end.sh
+++ f_end.sh
@@ -5,9 +5,19 @@ PRINT() {
   echo  -e "$1\t\t..."
 }
 
+STAT_CHECK(){
+  if [ $? -eq 0 ]; then
+    echo "\e[32mDone..\e[0m"
+  else
+    echo "\e[31mFail..\e[0m"
+    echo "\e[31m Check the log file for more details, Log File - $LOG\e[0m"
+  fi
+}
+
 PRINT "WHILE LOOP"
 p=4
 while [ $p -ne 0 ]; do
   PRINT "value of p = $p"
   p=$(($p-1))
 done
+STAT_CHECK $?
