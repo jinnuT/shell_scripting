afa9a61ea0e7054a31f76a938817df7e638e9972
jinnuT <jinnuaz@hotmail.com>
2021-07-22 12:10:59 -0700

    f_end.sh removing -n

--- f_end.sh
+++ f_end.sh
@@ -2,7 +2,7 @@
 LOG=/tmp/roboshop.log
 PRINT() {
   echo -e "\n###########################\t$1\t###########################" &>>$LOG
-  echo  -n -e "$1\t\t..."
+  echo  -e "$1\t\t..."
 }
 
 PRINT "WHILE LOOP"
