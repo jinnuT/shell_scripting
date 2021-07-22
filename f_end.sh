1a555b4797b68e379fff484daab78379b2c00992
jinnuT <jinnuaz@hotmail.com>
2021-07-22 12:10:09 -0700

    f_end.sh the code for while loop

--- f_end.sh
+++ f_end.sh
@@ -2,7 +2,12 @@
 LOG=/tmp/roboshop.log
 PRINT() {
   echo -e "\n###########################\t$1\t###########################" &>>$LOG
-  echo  -e "$1\t\t..."
+  echo  -n -e "$1\t\t..."
 }
 
 PRINT "WHILE LOOP"
+p=4
+while [ $p -ne 0 ]; do
+  PRINT "value of p = $p"
+  p=$(($p-1))
+done
