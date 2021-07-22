8bf53337fa11925a3cd585a713433fb6a897d3f6
jinnuT <jinnuaz@hotmail.com>
2021-07-21 23:47:47 -0700

    rabbitmq update 1

--- roboshop/rabbitmq.sh
+++ roboshop/rabbitmq.sh
@@ -2,22 +2,22 @@
 
 source common.sh
 
-PRINT "Install ERLang\t"
+PRINT "Install ERLang\t\t"
 yum list installed | grep erlang &>>$LOG
 if [ $? -ne 0 ]; then
   yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>$LOG
 fi
 STAT_CHECK $?
 
-PRINT "Set-up RabbitMQ repos"
+PRINT "Set-up RabbitMQ repos\t"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash  &>>$LOG
 STAT_CHECK $?
 
-PRINT "Install RabbitMQ Server"
+PRINT "Install RabbitMQ Server\t"
 yum install rabbitmq-server -y &>>$LOG
 STAT_CHECK $?
 
-PRINT "Start RabbitMQ Service"
+PRINT "Start RabbitMQ Service\t"
 systemctl enable rabbitmq-server &>>$LOG && systemctl start rabbitmq-server &>>$LOG
 STAT_CHECK $?
 
