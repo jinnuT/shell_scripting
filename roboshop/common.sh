2c13b0c57f5c23885c6155c7ab6b5196a031a009
jinnuT <jinnuaz@hotmail.com>
2021-07-22 09:14:08 -0700

    updating again

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -56,7 +56,7 @@ SETUP_SYSTEMD(){
   STAT_CHECK $?
 
   PRINT "Start ${COMPONENT} Service\t"
-  systemctl daemon-reload &>>$LOG && systemctl restart ${COMPONENT} &>>$LOG && systemctl enable ${COMPONENT} &>>$LOG
+  systemctl daemon-reload &>>$LOG && systemctl start ${COMPONENT} &>>$LOG && systemctl enable ${COMPONENT} &>>$LOG
   STAT_CHECK $?
 }
 #-------------------------------------------------------------------------------------------------------------------------#
